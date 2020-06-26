f35e5d4271f4a8f36412a488ef7e7adc44a48bc3
imjosephhong <imjosephhong@gmail.com>
2020-06-27 06:21:37 +0900

    Add the unit test for dispense API (In progress)

--- src/test/java/com/kakaopay/wallet/controller/WalletTests.java
+++ src/test/java/com/kakaopay/wallet/controller/WalletTests.java
@@ -1,5 +1,12 @@
 package com.kakaopay.wallet.controller;
 
+import java.util.UUID;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import org.jmock.lib.concurrent.Blitzer;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.slf4j.Logger;
@@ -16,6 +23,10 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.util.StopWatch;
 
+import com.cmm.util.CommonUtil;
+import com.kakaopay.wallet.model.wallet_dispense.WalletDispenseUTO;
+import com.kakaopay.wallet.model.wallet_transfer.WalletTransferCTO;
+
 @AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
@@ -26,21 +37,126 @@ public class WalletTests {
 	@Autowired
 	protected MockMvc mockMvc;
 
-	@Test
-	void walletTransferList() throws Exception {
+	static AtomicInteger c;
+	static Blitzer blitzer = new Blitzer(999, 10);
 
-		logger.debug("Start of walletTransferList ++++++++++++++++++++++++++++++++++++++");
+	@BeforeAll
+	static void setup() {
+		c = new AtomicInteger(0);
+	}
+
+	@AfterAll
+    static void tearDown() throws InterruptedException {
+        blitzer.shutdown();
+	}
+
+	@Disabled("Pending")
+	@Test
+	void transfer() throws Exception {
+
+		logger.debug("Start of transfer ++++++++++++++++++++++++++++++++++++++");
 		StopWatch sw = new StopWatch();
 		sw.start();
 
-		/** @formatter:off */
-		this.mockMvc.perform(MockMvcRequestBuilders.get("/wallet/v1/wallet_transfer/list"))
-													.andDo(MockMvcResultHandlers.print())
-													.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
-													.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE));
-		/** @formatter:on */
+		blitzer.blitz(new Runnable() {
+            public void run() {
+                int uniqueNo = c.getAndIncrement() + 1;
+
+                int userId = CommonUtil.randomNumeric(0, 10); //뿌리기 테스터 10명
+        		String roomId = "a001"; //대화방
+
+        		WalletTransferCTO walletTransferCTO = new WalletTransferCTO();
+        		//walletTransferCTO.setToken(String.format("a%02d", uniqueNo));
+        		walletTransferCTO.setToken(UUID.randomUUID().toString().substring(0, 3));
+        		walletTransferCTO.setAmount(100000.0);
+        		walletTransferCTO.setRecipientCount(20); //20명에게
+
+				try {
+	        		/** @formatter:off */
+	        		mockMvc.perform(MockMvcRequestBuilders.post("/wallet/v1/transfer")
+	        												.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+	        												.header("X-USER-ID", userId)
+	        												.header("X-ROOM-ID", roomId)
+	        												.content(walletTransferCTO.serialize())
+	        						)
+	        					.andDo(MockMvcResultHandlers.print())
+	        					.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+	        					.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE));
+	        		/** @formatter:on */
+
+				} catch(Exception e) {
+					logger.error("{}", e.fillInStackTrace());
+				}
+            }
+		});
 
 		sw.stop();
-		logger.debug("End of walletTransferList : {} --------------------------------------", sw.getTotalTimeSeconds());
+		logger.debug("End of transfer : {} --------------------------------------", sw.getTotalTimeSeconds());
+	}
+
+	@Test
+	void distributeBlockingInDupUserId() throws Exception {
+
+		logger.debug("Start of distributeBlockingInDupUserId ++++++++++++++++++++++++++++++++++++++");
+		StopWatch sw = new StopWatch();
+		sw.start();
+
+        int uniqueNo = c.getAndIncrement() + 1;
+
+        int userId = 2; //받기 - 동일한 유저
+		String roomId = "a001"; //대화방
+
+		WalletDispenseUTO walletDispenseUTO = new WalletDispenseUTO();
+		walletDispenseUTO.setToken("1c9");
+
+		/** @formatter:off */
+		this.mockMvc.perform(MockMvcRequestBuilders.put("/wallet/v1/dispense")
+													.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+													.header("X-USER-ID", userId)
+													.header("X-ROOM-ID", roomId)
+													.content(walletDispenseUTO.serialize())
+							)
+					.andDo(MockMvcResultHandlers.print())
+					.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+					.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE));
+		/** @formatter:on */
+
+
+		sw.stop();
+		logger.debug("End of distributeBlockingInDupUserId : {} --------------------------------------", sw.getTotalTimeSeconds());
+	}
+
+	@Disabled("Pending")
+	@Test
+	void distribute() throws Exception {
+
+		logger.debug("Start of distribute ++++++++++++++++++++++++++++++++++++++");
+		StopWatch sw = new StopWatch();
+		sw.start();
+
+
+        int uniqueNo = c.getAndIncrement() + 1;
+
+        int userId = CommonUtil.randomNumeric(0, 10); //받기 테스터 10명
+		String roomId = "a001"; //대화방
+
+		WalletDispenseUTO walletDispenseUTO = new WalletDispenseUTO();
+		walletDispenseUTO.setToken(UUID.randomUUID().toString().substring(0, 3));
+
+		/** @formatter:off */
+		this.mockMvc.perform(MockMvcRequestBuilders.put("/wallet/v1/dispense")
+													.header("Content-Type", MediaType.APPLICATION_JSON_VALUE)
+													.header("X-USER-ID", userId)
+													.header("X-ROOM-ID", roomId)
+													.content(walletDispenseUTO.serialize())
+							)
+					.andDo(MockMvcResultHandlers.print())
+					.andExpect(MockMvcResultMatchers.status().is(HttpStatus.OK.value()))
+					.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_VALUE));
+		/** @formatter:on */
+
+
+		sw.stop();
+		logger.debug("End of distribute : {} --------------------------------------", sw.getTotalTimeSeconds());
 	}
 }
